9b6778f23beb062278bda68a22f80bae9924d3ec
Fhme Ghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2019-07-09 22:17:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,24 +6,7 @@ const prefix = "$"; //تعريف البرفك
 bot.on('guildMemberAdd', member => {
    bot.channels.get('596473681736368130').send(member.id); 
    bot.channels.get('596473681736368130').send("Hi boys !!@#!@3"); 
-});
-bot.on('ready',async () => {
-  bot.channels.find(ch => ch.id === "529746544975544342" && ch.type === 'voice').join();
-  
-});
 
-  bot.on('message', msg => {
-    if(msg.content === "joinah"){
-      bot.channels.find(ch => ch.id === "529746544975544342" && ch.type === 'voice').join();
-    }
-  })
-
-  bot.on('message', msg => {
-    if(msg.author.id !== "595688805873483795") return;
-    if(msg.content === "leaveah"){
-      bot.channels.find(ch => ch.id === "529746544975544342" && ch.type === 'voice').leaveVoiceChannel;
-    }
-  })
 
 
 bot.login(process.env.BOT_TOKEN);
